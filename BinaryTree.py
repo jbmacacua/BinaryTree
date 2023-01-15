fc06ca2982338856cd3fd16781e0320c227dce07
Jonas Brian R. Macacua <jbrmacacua03@gmail.com>
2023-01-15 10:40:00 +0800

    add the delete function

--- BinaryTree.py
+++ BinaryTree.py
@@ -50,7 +50,28 @@ class BinarySearchTreeNode:
 
         return elements
 
-#======================Exercise Number 4 P1========================
+    def delete(self, val):
+        if val < self.data:
+            if self.left:
+                self.left = self.left.delete(val)
+        elif val > self.data:
+            if self.right:
+                self.right = self.right.delete(val)
+        else:
+            if self.left is None and self.right is None:
+                return None
+            elif self.left is None:
+                return self.right
+            elif self.right is None:
+                return self.left
+
+            min_val = self.right.find_min()
+            self.data = min_val
+            self.right = self.right.delete(min_val)
+
+        return
+
+    #======================Exercise Number 4 P1========================
 
     def post_order_traversal(self):
         elements = []
