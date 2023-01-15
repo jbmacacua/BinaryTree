6c93969a19978d0edc2a4ff5a41f062d73245e62
Jonas Brian R. Macacua <jbrmacacua03@gmail.com>
2023-01-15 09:11:18 +0800

    add data in the left subtree using if else statement

--- BinaryTree.py
+++ BinaryTree.py
@@ -7,3 +7,10 @@ class BinarySearchTreeNode:
     def add_child(self, data):
         if data == self.data:
             return
+
+        if data < self.data:
+            if self.left:
+                self.left.add_child(data)
+            else:
+                self.left = BinarySearchTreeNode(data)
+
