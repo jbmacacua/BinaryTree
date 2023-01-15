ea3a71e970f8bbc417c4a3d94dec7a9804150fc4
Jonas Brian R. Macacua <jbrmacacua03@gmail.com>
2023-01-15 09:07:35 +0800

    add child data

--- BinaryTree.py
+++ BinaryTree.py
@@ -3,3 +3,7 @@ class BinarySearchTreeNode:
         self.data = data
         self.left = None
         self.right = None
+
+    def add_child(self, data):
+        if data == self.data:
+            return
