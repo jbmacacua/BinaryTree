9914e06127b00bbaf05d55a70915966b29c6c0df
Jonas Brian R. Macacua <jbrmacacua03@gmail.com>
2023-01-15 09:25:44 +0800

    Build tree

--- BinaryTree.py
+++ BinaryTree.py
@@ -32,4 +32,13 @@ class BinarySearchTreeNode:
 
         return elements
 
+def build_tree(elements):
+    print("Building tree with these elements:",elements)
+    root = BinarySearchTreeNode(elements[0])
+
+    for i in range(1,len(elements)):
+        root.add_child(elements[i])
+
+    return root
+
 
